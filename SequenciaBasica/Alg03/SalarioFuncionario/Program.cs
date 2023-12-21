7007861b2a8d2457329c9e94aa82b52ae0a74c0e
FelipeF-soares <felipe.fends@gmail.com>
2023-12-21 14:39:37 -0300

    21/12/2023

--- SequenciaBasica/Alg03/SalarioFuncionario/Program.cs
+++ SequenciaBasica/Alg03/SalarioFuncionario/Program.cs
@@ -3,8 +3,8 @@ using SalarioFuncionario.view;
 
 MensagensExibicao mensagem = new MensagensExibicao();
 Pessoa pessoa = new Pessoa();
-System.Console.WriteLine(mensagem.SolicitaNome());
+Console.WriteLine(mensagem.SolicitaNome());
 pessoa.Nome = Console.ReadLine();
-System.Console.WriteLine(mensagem.SolicitaSalario());
+Console.WriteLine(mensagem.SolicitaSalario());
 pessoa.Salario = decimal.Parse(Console.ReadLine());
-System.Console.WriteLine(mensagem.ExibiNomeSalario(pessoa));
+Console.WriteLine(mensagem.ExibiNomeSalario(pessoa));
--- SequenciaBasica/Alg03/SalarioFuncionarioTest/SalarioFuncionarioTests.cs
+++ SequenciaBasica/Alg03/SalarioFuncionarioTest/SalarioFuncionarioTests.cs
@@ -1,14 +1,34 @@
 /*
     Crie um programa que leia o nome e o salário de um funcionário, mostrando no final uma mensagem. 
-    Ex: Nome do Funcionário: Maria do Carmo Salário: 1850,45 O funcionário Maria do Carmo tem um salário de R$1850,45 em Junho.
+    Ex: Nome do Funcionário: Maria do Carmo Salário: 1850,45 O funcionário Maria do Carmo tem um salário de R$1.850,45 em Junho.
 */
+using System.Runtime.InteropServices;
+using SalarioFuncionario.service;
+using SalarioFuncionario.view;
+
 namespace SalarioFuncionarioTest;
 
 public class SalarioFuncionarioTests
 {
+    private Pessoa _pessoa;
+    private RetornaMes _mes;
+    private MensagensExibicao _mensagen;
+    public SalarioFuncionarioTests()
+    {
+        _pessoa = new Pessoa();
+        _mes = new RetornaMes();
+        _mensagen = new MensagensExibicao();
+    }
     [Fact]
     public void RetornaONomeEOSalarioDoMes()
     {
-
+        //Arrange
+        _pessoa.Nome = "Maria do Carmo";
+        _pessoa.Salario = 1850.45M;
+        string mes = _mes.ToString();
+        //Act
+        string resposta = _mensagen.ExibiNomeSalario(_pessoa);
+        //Assert
+        Assert.Equal($"O funcionário Maria do Carmo tem um salário de R$ 1.850,45 em {mes}.",resposta);
     }
 }
